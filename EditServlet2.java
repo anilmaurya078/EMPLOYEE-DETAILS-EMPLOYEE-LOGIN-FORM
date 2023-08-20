19fff9ba9484ed3220d7f077b3b7b04003984c20
ANIL  MAURYA <136904954+anilmaurya078@users.noreply.github.com>
2023-08-20 23:13:45 +0530

    Add files via upload

---
+++ EditServlet2.java
@@ -0,0 +1,42 @@
+package user;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@SuppressWarnings("serial")
+@WebServlet("/EditServlet2")
+public class EditServlet2 extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		String sid=request.getParameter("id");
+		int id=Integer.parseInt(sid);
+		String name=request.getParameter("name");
+		String department=request.getParameter("department");
+		String phone=request.getParameter("phone");
+		String country=request.getParameter("country");
+		
+		Emp e=new Emp();
+		e.setId(id);
+		e.setName(name);
+		e.setDepartment(department);
+		e.setPhone(phone);
+		e.setCountry(country);
+		
+		int status=EmpDao.update(e);
+		if(status>0){
+			response.sendRedirect("ViewServlet");
+		}else{
+			out.println("Sorry! unable to update record");
+		}
+		
+		out.close();
+	}
+
+}
---
+++ Emp.java
@@ -0,0 +1,37 @@
+package user;
+
+public class Emp {
+private int id;
+private String name,department,phone,country;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getDepartment() {
+	return department;
+}
+public void setDepartment(String department) {
+	this.department = department;
+}
+public String getPhone() {
+	return phone;
+}
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+public String getCountry() {
+	return country;
+}
+public void setCountry(String country) {
+	this.country = country;
+}
+
+}
